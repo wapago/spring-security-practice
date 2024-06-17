8ebad1cfa8c2aa043544a30f4237d0d599b9fe2f
Kim Chanho <beroso20th@gmail.com>
2024-06-18 05:33:16 +0900

    second commit

---
+++ src/test/java/com/github/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
